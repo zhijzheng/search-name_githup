8d91cbe4f8c9fb1cfe5b98f8a6f9c8faa7adfa45
v_zhijzheng <v_zhijzheng@tencent.com>
2022-08-20 17:06:40 +0800

    fix: 新增ap查询接口

--- search_name_githup/src/Seach/index.jsx
+++ search_name_githup/src/Seach/index.jsx
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
 import { Input, Button } from 'antd'
+import axios from 'axios'
 import './index.css'
-
 export default class Search extends Component {
-    
+  state ={
+    inputValue:'',
+  }
+  //用户输入值
+  inputChage=(e) =>{
+    this.setState({
+      inputValue: e.target.value,
+    })
+
+  }
+    search=()=>{
+    const serchVlaue = this.state.inputValue
+    axios.get(`https://api.github.com/search/users?q=${serchVlaue}`).then(
+      response => {console.log(response.data,'成功了')},
+      error => {console.log(error,'失败了')}
+    );
+
+    }
   render() {
     return (
       <div>
-        <Input/>
-        <Button>搜索</Button>
+        <Input onChange={this.inputChage} value={this.state.inputValue} type='text' placeholder='请输入需查询用户'/>
+        <Button onClick={this.search}>搜索</Button>
       </div>
     )
   }
